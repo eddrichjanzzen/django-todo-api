14c9584dc54323fab1594c202eab1fadaac6e4c3
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-23 20:53:10 +0800

    refactor urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from tasks import views
+from tasks.views import api_root_view, task_view, user_view 
 
 urlpatterns = [
-    path('tasks/', views.task_list),
-    path('tasks/<uuid:pk>/', views.task_detail),
+    path('tasks/', task_view.TaskList.as_view(), name='task-list'),
+    path('tasks/<uuid:pk>/', task_view.TaskDetail.as_view()),
+    path('users/', user_view.UserList.as_view(), name='user-list'),
+    path('users/<uuid:pk>/', user_view.UserDetail.as_view()),
 ]
