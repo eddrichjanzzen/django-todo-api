2b2bbd92ec4bd5d17e759d56c8fd905e63179925
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-23 20:52:29 +0800

    refactor serializers

---
+++ tasks/serializers/task_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from tasks.models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['id', 'title', 'description', 'owner', 'deadline', 'created_date', 'updated_date', 'completed']
+        
