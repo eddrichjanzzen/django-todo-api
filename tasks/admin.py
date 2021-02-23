5a56ecf44c728dcbe64a90db64c417d9f2f8814e
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-23 23:30:30 +0800

    set up basic admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+	"""Define admin model for custom User model with no email field."""
+
+	fieldsets = (
+		(None, {'fields': ('email', 'password')}),
+
+	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email', 'password1', 'password2'),
+		}),
+	)
+	list_filter = ('is_staff', 'is_superuser', 'is_active')
+	list_display = ('email', 'is_staff')
+	search_fields = ('email',)
+	ordering = ('email',)
--- todoApi/urls.py
+++ todoApi/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from tasks.views import api_root_view
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('', api_root_view.api_root, name='index'),
+    path('admin/', admin.site.urls),
     path('api/', include('tasks.urls')),
     path('api/auth/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
