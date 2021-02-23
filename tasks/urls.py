9ec2bf3ea57c39bd8636f448604071ca54177752
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-24 01:08:30 +0800

    clean up spacing

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from tasks.views import api_root_view, task_view, user_view 
 
 urlpatterns = [
-    path('tasks/', task_view.TaskList.as_view(), name='task-list'),
+    path('tasks', task_view.TaskList.as_view(), name='task-list'),
     path('tasks/<uuid:pk>/', task_view.TaskDetail.as_view()),
-    path('users/', user_view.UserList.as_view(), name='user-list'),
+    path('users', user_view.UserList.as_view(), name='user-list'),
     path('users/<uuid:pk>/', user_view.UserDetail.as_view()),
 ]
