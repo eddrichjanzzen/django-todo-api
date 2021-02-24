78536c1ecb535cd19357fe00da7b24ed5bac24c1
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-25 01:28:43 +0800

    add user signup and login endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from tasks.views import api_root_view, task_view, user_view 
 
 urlpatterns = [
-    path('tasks', task_view.TaskList.as_view(), name='task-list'),
-    path('tasks/<uuid:pk>/', task_view.TaskDetail.as_view()),
-    path('users', user_view.UserList.as_view(), name='user-list'),
-    path('users/<uuid:pk>/', user_view.UserDetail.as_view()),
+    path('task', task_view.TaskList.as_view(), name='task-list'),
+    path('task/<uuid:pk>/', task_view.TaskDetail.as_view()),
+    path('user', user_view.UserList.as_view(), name='user-list'),
+    path('user/signup', user_view.UserSignUp.as_view(), name='user-signup'),
+    path('user/login', user_view.UserLogin.as_view(), name='user-login'),
+    path('user/me', user_view.UserGetDetails.as_view()),
 ]
