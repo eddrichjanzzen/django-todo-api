c9110b21a0c6714c1d91b78a9b177bae1adceb17
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-25 01:30:22 +0800

    update user views

--- tasks/views/user_view.py
+++ tasks/views/user_view.py
@@ -6,10 +6,26 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserList(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
     queryset = User.objects.all()
     serializer_class = user_serializer.UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserSignUp(generics.CreateAPIView):
+    model = User
     queryset = User.objects.all()
+    serializer_class = user_serializer.UserSignUpSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserLogin(generics.CreateAPIView):
+    model = User
+    queryset = User.objects.all()
+    serializer_class = user_serializer.UserLoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+class UserGetDetails(generics.RetrieveAPIView):
     serializer_class = user_serializer.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user)
