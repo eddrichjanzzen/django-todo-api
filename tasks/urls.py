b14ecfdecc6befeb47555c85440392d0008ecf8c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-25 16:57:09 +0800

    update url regEx for tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from tasks.views import api_root_view, task_view, user_view 
+from django.conf.urls import url
 
 urlpatterns = [
     path('task', task_view.TaskList.as_view(), name='task-list'),
-    path('task/<uuid:pk>/', task_view.TaskDetail.as_view()),
+    # accept match any regEx after 'task/'
+    url(r'^task\/(?P<pk>.+)', task_view.TaskDetail.as_view()),
     path('user', user_view.UserList.as_view(), name='user-list'),
     path('user/signup', user_view.UserSignUp.as_view(), name='user-signup'),
     path('user/login', user_view.UserLogin.as_view(), name='user-login'),
-    path('user/me', user_view.UserGetDetails.as_view()),
+    path('user/me', user_view.UserGetProfileDetails.as_view(), name='user-me'),
+    path('user/update/me', user_view.UserUpdateProfileDetails.as_view(), name='user-update-me'),
 ]
