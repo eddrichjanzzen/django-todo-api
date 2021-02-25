c7d8322bd8b7d8e8fe06268a2f52dd730085f449
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-25 16:56:41 +0800

    add filter task by title functionality

--- tasks/views/task_view.py
+++ tasks/views/task_view.py
@@ -1,19 +1,70 @@
 from tasks.models import Task
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from tasks.serializers import task_serializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class TaskList(generics.ListCreateAPIView):
 
     serializer_class = task_serializer.TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    
     def get_queryset(self):
-        return Task.objects.filter(owner=self.request.user)
+
+        title = self.request.query_params.get('title', None)
+        tasks = Task.objects.filter(owner=self.request.user)
+
+        # filter by title
+        if title is not None: 
+            tasks = tasks.filter(title__icontains=title)
+
+        return tasks
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        response = {
+            'success' : True,
+            'data': serializer.data
+        }
+
+        return Response(response, status=status.HTTP_201_CREATED)
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = task_serializer.TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        
+        instance = self.get_object()
+        serializer = task_serializer.TaskDetailSerializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        response = {
+            'success': True,
+            'data': serializer.data
+        }
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        response = {
+            'success': True,
+            'data': serializer.data 
+        }
+
+        return Response(response, status=status.HTTP_200_OK)
