dd22ac766dc92ad3e29b85930d462b6c7f38730c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-25 20:12:30 +0800

    add upload avatar view

--- tasks/views/user_view.py
+++ tasks/views/user_view.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, views
 from tasks.serializers import user_serializer
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.parsers import MultiPartParser, JSONParser
+import cloudinary.uploader
 
 User = get_user_model()
 
 class UserList(generics.ListAPIView):
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.AllowAny]
     queryset = User.objects.all()
     serializer_class = user_serializer.UserSerializer
 
@@ -30,7 +32,7 @@ class UserLogin(generics.GenericAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class UserGetProfileDetails(generics.RetrieveAPIView):
+class UserGetProfileDetail(generics.RetrieveAPIView):
     serializer_class = user_serializer.UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -42,7 +44,7 @@ class UserGetProfileDetails(generics.RetrieveAPIView):
         obj = get_object_or_404(queryset, id=self.request.user.id)
         return obj
 
-class UserUpdateProfileDetails(generics.UpdateAPIView):
+class UserUpdateProfileDetail(generics.UpdateAPIView):
     serializer_class = user_serializer.UserUpdateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -56,13 +58,62 @@ class UserUpdateProfileDetails(generics.UpdateAPIView):
 
     def update(self, request, *args, **kwargs):
         
-        serializer = self.get_serializer(data=request.data)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
-        
-        super(UserUpdateProfileDetails, self).update(request, * args, **kwargs)
+        self.perform_update(serializer)        
 
         response = {
-            'success': True 
+            'success': True,
+            'data': serializer.data
         }
 
         return Response(response, status=status.HTTP_200_OK)
+
+
+class UserUploadAvatar(generics.GenericAPIView):
+    model = User
+    serializer_class = user_serializer.UserUploadAvatarSerializer
+    permission_classes = [permissions.AllowAny]
+
+    parser_classes = (
+        MultiPartParser,
+        JSONParser
+    )
+
+    def post(self, request):
+        file = request.data.get('avatar', None)
+        success = False
+        upload_data = {}
+
+        try:
+            upload_data = cloudinary.uploader.upload(file)
+            success = True
+
+        except Exception as e:
+            print(e)
+            success = False
+            upload_data = {}
+
+        response = {
+            'success': success,
+            'data': upload_data
+        }
+
+        return Response(response, status=status.HTTP_200_OK)
+
+class UploadView(views.APIView):
+    parser_classes = (
+        MultiPartParser,
+        JSONParser,
+    )
+
+    @staticmethod
+    def post(request):
+        file = request.data.get('avatar')
+
+        upload_data = cloudinary.uploader.upload(file)
+        return Response({
+            'status': 'success',
+            'data': upload_data,
+        }, status=201)
