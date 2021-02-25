60e8dbef0d5c7325157dacdf98ab7daf98bbacea
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-26 01:50:11 +0800

    define upload cloudinary and delete cloudinary

--- tasks/views/user_view.py
+++ tasks/views/user_view.py
@@ -5,7 +5,9 @@ from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.parsers import MultiPartParser, JSONParser
+from cloudinary import CloudinaryResource
 import cloudinary.uploader
+
 
 User = get_user_model()
 
@@ -30,7 +32,6 @@ class UserLogin(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
-
 
 class UserGetProfileDetail(generics.RetrieveAPIView):
     serializer_class = user_serializer.UserSerializer
@@ -70,50 +71,90 @@ class UserUpdateProfileDetail(generics.UpdateAPIView):
 
         return Response(response, status=status.HTTP_200_OK)
 
-
-class UserUploadAvatar(generics.GenericAPIView):
+class UserUploadDeleteAvatar(generics.GenericAPIView):
     model = User
     serializer_class = user_serializer.UserUploadAvatarSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
-    parser_classes = (
+    parser_classes = [
         MultiPartParser,
         JSONParser
-    )
+    ]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+    
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, id=self.request.user.id)
+        return obj
 
     def post(self, request):
+
         file = request.data.get('avatar', None)
         success = False
         upload_data = {}
 
         try:
-            upload_data = cloudinary.uploader.upload(file)
+            upload_data = cloudinary.uploader.upload(
+                    file, 
+                    folder='todo_avatars/'
+                )
+
+            # update the user avatar url
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data={
+                'avatar': upload_data['url']
+            })
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
             success = True
 
         except Exception as e:
-            print(e)
-            success = False
-            upload_data = {}
+            response = {
+                'success': success,
+                'error': 'A problem occured when uploading file: {}'.format(e)
+            }
+
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
         response = {
             'success': success,
-            'data': upload_data
+            'data': {
+                'name': upload_data['public_id'],
+                'url': upload_data['url']
+            }
         }
 
         return Response(response, status=status.HTTP_200_OK)
 
-class UploadView(views.APIView):
-    parser_classes = (
-        MultiPartParser,
-        JSONParser,
-    )
+    def delete(self, request):
+        success = False
+        try:
 
-    @staticmethod
-    def post(request):
-        file = request.data.get('avatar')
+            instance = self.get_object()
+            print(instance.cloudinary_public_id())
+            cloudinary.uploader.destroy(instance.cloudinary_public_id(), invalidate=True)
 
-        upload_data = cloudinary.uploader.upload(file)
-        return Response({
-            'status': 'success',
-            'data': upload_data,
-        }, status=201)
+            serializer = self.get_serializer(instance, data={
+                'avatar': ''
+            })
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            success = True
+
+        except Exception as e:
+            response = {
+                'success': success,
+                'error': 'A problem occured while deleting file: {}'.format(e)
+            }
+
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+        response = {
+            'success': success
+        }
+
+        return Response(response, status=status.HTTP_200_OK)
