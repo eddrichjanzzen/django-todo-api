7c408333952515cb7e21bbca75a9d8e04ba6792d
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-27 16:37:39 +0800

    additional configurations for static files

--- todoApi/settings.py
+++ todoApi/settings.py
@@ -201,5 +201,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
