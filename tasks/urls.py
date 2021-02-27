dd9a2983214dc7b93a47c4c8db312ab62a48c2a2
Eddrich Janzzen Ang <eddrichang@gmail.com>
2021-02-27 16:13:09 +0800

    add requirements for heroku deployment

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from tasks.views import api_root_view, task_view, user_view 
 from django.conf.urls import url
 
 urlpatterns = [
     path('task', task_view.TaskList.as_view(), name='task-list'),
     # accept match any regEx after 'task/'
-    url(r'^task\/(?P<pk>.+)$', task_view.TaskDetail.as_view()),
+    re_path(r'^task\/(?P<pk>.+)$', task_view.TaskDetail.as_view()),
     path('user', user_view.UserList.as_view(), name='user-list'),
     path('user/signup', user_view.UserSignUp.as_view(), name='user-signup'),
     path('user/login', user_view.UserLogin.as_view(), name='user-login'),
--- todoApi/settings.py
+++ todoApi/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from decouple import config
 import cloudinary
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +34,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'app-drf-todo-api.herokuapp.com'
+]
 
 
 # Application definition
@@ -56,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -186,4 +190,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
